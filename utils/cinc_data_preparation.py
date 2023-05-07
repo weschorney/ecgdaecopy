6514bf3750cfafe296a88c2d394671ad5be9fcf3
Wes Chorney <wes.chorney@gmail.com>
2023-05-06 19:23:52 -0700

    full split cinc data

--- utils/cinc_data_preparation.py
+++ utils/cinc_data_preparation.py
@@ -14,27 +14,76 @@ import wfdb
 from scipy import signal
 
 def prepare_cinc(seed=777):
+    np.random.seed(seed=seed)
     nstdb = load_noise()
     [bw_signals, em_signals, ma_signals] = nstdb
     bw_signals = np.array(bw_signals)
     em_signals = np.array(em_signals)
     ma_signals = np.array(ma_signals)
-
-
-    bw_noise_channel1_a = bw_signals[0:int(bw_signals.shape[0]/2), 0]
-    bw_noise_channel1_b = bw_signals[int(bw_signals.shape[0]/2):-1, 0]
-    bw_noise_channel2_a = bw_signals[0:int(bw_signals.shape[0]/2), 1]
-    bw_noise_channel2_b = bw_signals[int(bw_signals.shape[0]/2):-1, 1]
-
-    em_noise_channel1_a = em_signals[0:int(em_signals.shape[0]/2), 0]
-    em_noise_channel1_b = em_signals[int(em_signals.shape[0]/2):-1, 0]
-    em_noise_channel2_a = em_signals[0:int(em_signals.shape[0]/2), 1]
-    em_noise_channel2_b = em_signals[int(em_signals.shape[0]/2):-1, 1]
-
-    ma_noise_channel1_a = ma_signals[0:int(ma_signals.shape[0]/2), 0]
-    ma_noise_channel1_b = ma_signals[int(ma_signals.shape[0]/2):-1, 0]
-    ma_noise_channel2_a = ma_signals[0:int(ma_signals.shape[0]/2), 1]
-    ma_noise_channel2_b = ma_signals[int(ma_signals.shape[0]/2):-1, 1]
+    #cinc is lead1 equiv so use noise from there
+    noise_train = bw_signals[0:int(bw_signals.shape[0]/2), 0]
+    noise_test = bw_signals[int(bw_signals.shape[0]/2):-1, 0]
+    noise_train2 = em_signals[0:int(em_signals.shape[0]/2), 0]
+    noise_test2 = em_signals[int(em_signals.shape[0]/2):-1, 0]
+    noise_train3 = ma_signals[0:int(ma_signals.shape[0]/2), 0]
+    noise_test3 = ma_signals[int(ma_signals.shape[0]/2):-1, 0]
+    #load train and test
+    beats_train = load_train_data()
+    beats_test = load_test_data()
+    #split beats
+    beats_train = sigs_to_parts(beats_train)
+    beats_test = sigs_to_parts(beats_test)
+    sn_train = []
+    sn_test = []
+    samples = 512
+    noise_index = 0
+    # Adding noise to train
+    rnd_train = np.random.randint(low=20, high=200, size=len(beats_train)) / 100
+    for i in range(len(beats_train)):
+        noise = noise_train[noise_index:noise_index + samples]
+        noise2 = noise_train2[noise_index:noise_index + samples]
+        noise3 = noise_train3[noise_index:noise_index + samples]
+        noise = noise + noise2 + noise3
+        beat_max_value = np.max(beats_train[i]) - np.min(beats_train[i])
+        noise_max_value = np.max(noise) - np.min(noise)
+        Ase = noise_max_value / beat_max_value
+        alpha = rnd_train[i] / Ase
+        signal_noise = beats_train[i] + alpha * noise
+        sn_train.append(signal_noise)
+        noise_index += samples
+        if noise_index > (len(noise_train) - samples):
+            noise_index = 0
+    # Adding noise to test
+    noise_index = 0
+    rnd_test = np.random.randint(low=20, high=200, size=len(beats_test)) / 100
+    # Saving the random array so we can use it on the amplitude segmentation tables
+    np.save('rnd_test_cinc.npy', rnd_test)
+    print('rnd_test shape: ' + str(rnd_test.shape))
+    for i in range(len(beats_test)):
+        noise = noise_test[noise_index:noise_index + samples]
+        noise2 = noise_test2[noise_index:noise_index + samples]
+        noise3 = noise_test3[noise_index:noise_index + samples]
+        noise = noise + noise2 + noise3
+        beat_max_value = np.max(beats_test[i]) - np.min(beats_test[i])
+        noise_max_value = np.max(noise) - np.min(noise)
+        Ase = noise_max_value / beat_max_value
+        alpha = rnd_test[i] / Ase
+        signal_noise = beats_test[i] + alpha * noise
+        sn_test.append(signal_noise)
+        noise_index += samples
+        if noise_index > (len(noise_test) - samples):
+            noise_index = 0
+    X_train = np.array(sn_train)
+    y_train = np.array(beats_train)
+    X_test = np.array(sn_test)
+    y_test = np.array(beats_test)
+    X_train = np.expand_dims(X_train, axis=2)
+    y_train = np.expand_dims(y_train, axis=2)
+    X_test = np.expand_dims(X_test, axis=2)
+    y_test = np.expand_dims(y_test, axis=2)
+    Dataset = [X_train, y_train, X_test, y_test]
+    with open('../data/dataset_cinc.pkl', 'wb') as f:
+        pickle.dump(Dataset, f)
     return
 
 def resample_signal(x, fs, fs_target):
@@ -53,13 +102,23 @@ def resample_signal(x, fs, fs_target):
     assert np.all(np.diff(resampled_t) > 0)
     return resampled_x, resampled_t
 
-def load_data(my_fs=250):
+def load_train_data(my_fs=250):
     signals = []
     for folder_name in [f'A0{i}' for i in range(9)]:
         for sig in glob.glob(f'../data/cinc2017/training/{folder_name}/*.hea'):
             my_sig = wfdb.rdsamp(sig[:-4])
-            my_sig = resample_signal(my_sig[0], my_sig[1]['fs'], my_fs)
+            my_sig, _ = resample_signal(my_sig[0], my_sig[1]['fs'], my_fs)
+            my_sig = my_sig.reshape((-1,))
             signals.append(my_sig)
+    return signals
+
+def load_test_data(my_fs=250):
+    signals = []
+    for sig in glob.glob('../data/cinc2017/validation/*.hea'):
+        my_sig = wfdb.rdsamp(sig[:-4])
+        my_sig, _ = resample_signal(my_sig[0], my_sig[1]['fs'], my_fs)
+        my_sig = my_sig.reshape((-1,))
+        signals.append(my_sig)
     return signals
 
 def load_noise():
@@ -73,4 +132,11 @@ def sig_to_parts(sig, part_size=512):
         parts.append(sig[i*part_size:(i+1)*part_size])
     return parts
 
+def sigs_to_parts(sigs, part_size=512):
+    parts = []
+    for sig in sigs:
+        parts += sig_to_parts(sig, part_size=part_size)
+    return parts
 
+if __name__ == '__main__':
+    prepare_cinc()
