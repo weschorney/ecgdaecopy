1ca2dcd491457bb2fbc827e9ce5d37a2db9da954
Wes Chorney <wes.chorney@gmail.com>
2023-05-17 20:26:28 -0700

    remove unneeded files

--- deepFilter/__init__.py
+++
@@ -1,9 +0,0 @@
-#============================================================
-#
-#  FIXME: PROJECT NAME
-#
-#  author: Francisco Perdigon Romero
-#  github id: fperdigon
-#  MedICAL Lab
-#
-#============================================================
